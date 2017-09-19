2870e7f3823c0ba593a9873e22c0fadef0ec9ba7
kokh0716 <konstantin.kharlambov@netcracker.com>
2017-09-19 18:43:34 +0300

    Agent and rubbish parameters were added

--- location.c
+++ location.c
@@ -2,10 +2,20 @@
 #include <sys\stat.h>
 
 #define CELL_VALUE_LENGTH 4
+#define DEFAULT_RUBBISH_WEIGHT 1
+#define DEFAULT_AGENT_ENERGY 2000
 
 Cell* first_empty_cell(Location *location);
 
 void define_agent_coordinates(Location *location);
+
+void init_agent_for_file(Location *location, int players_count);
+
+int* rubbish_info(Location *location);
+
+void randomize_rubbish_for_file(Location *location, int rubbish_count);
+
+void display_agent_stat(Location *location);
 
 void move_top(Location *location);
 
@@ -77,14 +87,14 @@ void randomize_matrix(Location *location, int walls_count) {
     }  
 }
 
-void file_write_matrix(Location *location, char *file_path,
-     int walls_count, int rubbish_count, int players_count) {
+void file_write_matrix(Location *location, char *file_path, int walls_count,
+     int rubbish_count, int players_count) {
     FILE *file = fopen(file_path, "w");
     if(file != NULL) {
         init_matrix(location);
         randomize_matrix(location, walls_count);
-        randomize_rubbish(location, rubbish_count);
-        init_agent(location, players_count);
+        randomize_rubbish_for_file(location, rubbish_count);
+        init_agent_for_file(location, players_count);
         for(int i = 0; i < location->x_size; i++) {
             for(int j = 0; j < location->y_size; j++) {                 
                 fputs((char *) location->matrix[i][j].value, file);                                
@@ -107,12 +117,18 @@ void file_read_matrix(Location *location, char *file_path) {
                 if(strcmp(buffer, CELL_VALUE[WALL]) == 0) {                           
                     cell->tag = WALL;
                     cell->value = CELL_VALUE[WALL];               
-                } else if(strcmp(buffer, CELL_VALUE[RUBBISH]) == 0) {                  
+                } else if(strcmp(buffer, CELL_VALUE[RUBBISH]) == 0) {            
+                    Rubbish rubbish;
+                    rubbish.weight = DEFAULT_RUBBISH_WEIGHT;
                     cell->tag = RUBBISH;
                     cell->value = CELL_VALUE[RUBBISH]; 
-                } else if(strcmp(buffer, CELL_VALUE[AGENT]) == 0) {                  
+                    cell->rubbish = rubbish;
+                } else if(strcmp(buffer, CELL_VALUE[AGENT]) == 0) {      
+                    Agent agent;
+                    agent.energy_count = DEFAULT_AGENT_ENERGY;            
                     cell->tag = AGENT;
                     cell->value = CELL_VALUE[AGENT];        
+                    cell->agent = agent;
                 } else {                  
                     cell->tag = ROOM;
                     cell->value = CELL_VALUE[ROOM]; 
@@ -136,27 +152,42 @@ void define_agent_coordinates(Location *location) {
     }     
 }
 
-void randomize_rubbish(Location *location, int rubbish_count) {
+void randomize_rubbish_for_file(Location *location, int rubbish_count) {
     srand(time(NULL));
     for(int i = 0; i < rubbish_count; i++) {
         int row_index = (int) rand() % (location->x_size - 1);
         int col_index = (int) rand() % (location->y_size - 1);
-        if(location->matrix[row_index][col_index].tag != WALL) {
-            location->matrix[row_index][col_index].tag = RUBBISH;
+        if(location->matrix[row_index][col_index].tag != WALL) {          
             location->matrix[row_index][col_index].value = CELL_VALUE[RUBBISH];
         }        
     }  
 }
 
-void init_agent(Location *location, int players_count) {
+void randomize_rubbish(Location *location, int rubbish_count, int weight) {
+    srand(time(NULL));
+    for(int i = 0; i < rubbish_count; i++) {
+        int row_index = (int) rand() % (location->x_size - 1);
+        int col_index = (int) rand() % (location->y_size - 1);
+        if(location->matrix[row_index][col_index].tag != WALL) {
+            if(location->matrix[row_index][col_index].tag != RUBBISH) {
+                Rubbish rubbish;
+                rubbish.weight = weight;
+                location->matrix[row_index][col_index].rubbish = rubbish;
+                location->matrix[row_index][col_index].tag = RUBBISH;
+                location->matrix[row_index][col_index].value = CELL_VALUE[RUBBISH];
+            } else {
+                location->matrix[row_index][col_index].rubbish.weight++;
+            }          
+        }        
+    }  
+}
+
+void init_agent_for_file(Location *location, int players_count) {
     srand(time(NULL));
     for(int i = 0; i < players_count; i++) {
         int row_index = (int) rand() % (location->x_size - 1);
         int col_index = (int) rand() % (location->y_size - 1);
-        if(location->matrix[row_index][col_index].tag == ROOM) {
-            location->agent_coordinates[0] = row_index;
-            location->agent_coordinates[1] = col_index;
-            location->matrix[row_index][col_index].tag = AGENT;
+        if(location->matrix[row_index][col_index].tag == ROOM) {               
             location->matrix[row_index][col_index].value = CELL_VALUE[AGENT];
         } else {
             players_count++;
@@ -164,11 +195,68 @@ void init_agent(Location *location, int players_count) {
     }  
 }
 
+
+void init_agent(Location *location, int players_count, int energy_count) {
+    srand(time(NULL));
+    for(int i = 0; i < players_count; i++) {
+        int row_index = (int) rand() % (location->x_size - 1);
+        int col_index = (int) rand() % (location->y_size - 1);
+        if(location->matrix[row_index][col_index].tag == ROOM) {
+            Agent agent;
+            agent.energy_count = energy_count;
+            location->agent_coordinates[0] = row_index;
+            location->agent_coordinates[1] = col_index;
+            location->matrix[row_index][col_index].agent = agent;
+            location->matrix[row_index][col_index].tag = AGENT;            
+            location->matrix[row_index][col_index].value = CELL_VALUE[AGENT];
+        } else {
+            players_count++;
+        }       
+    }  
+}
+
+int* rubbish_info(Location *location) {
+    int *result = (int *) malloc(2 * sizeof(int));
+    int total_weigth = 0;
+    int count = 0;
+    for(int i = 0; i < location->x_size; i++) {       
+        for(int j = 0; j < location->y_size; j++) {            
+           if(location->matrix[i][j].tag == RUBBISH) {
+               count++;
+               total_weigth += location->matrix[i][j].rubbish.weight;
+           }
+        }             
+    }
+    result[0] = count;
+    result[1] = total_weigth;
+    return result;
+}
+
+void display_agent_stat(Location *location) {
+    printf("Agent X: %d; Agent Y: %d", location->agent_coordinates[0],
+     location->agent_coordinates[1]);    
+}
+
+void display_rubbish_stat(Location *location) {
+    int *rubbish = rubbish_info(location);
+    printf("Rubbish count: %d; Rubbish total weight: %d", rubbish[0], rubbish[1]);    
+    free(rubbish);
+}
+
 void display_matrix(Location *location) {
-    for(int i = 0; i < location->x_size; i++) {
-        for(int j = 0; j < location->y_size; j++) {
+    for(int i = 0; i < location->x_size; i++) {       
+        for(int j = 0; j < location->y_size; j++) {            
             printf("%s", location->matrix[i][j].value);
-        }
+            if(j == location->y_size - 1) {               
+                if(i == 0) {       
+                    printf("\t");            
+                    display_agent_stat(location);
+                } else if(i == 1) {
+                    printf("\t");
+                    display_rubbish_stat(location);
+                }
+            }          
+        }        
         printf("\n");
     }
 }
@@ -178,7 +266,9 @@ void move_right(Location *location) {
     int agent_y = location->agent_coordinates[1];
     if(location->matrix[agent_x][agent_y + 1].tag != WALL) {
         location->matrix[agent_x][agent_y].value = CELL_VALUE[ROOM];
+        Agent agent = location->matrix[agent_x][agent_y + 1].agent;
         location->matrix[agent_x][agent_y + 1].value = CELL_VALUE[AGENT]; 
+        location->matrix[agent_x][agent_y + 1].agent.energy_count--;
         location->agent_coordinates[1] = agent_y + 1; 
     } else {
         //move_left(location);
@@ -222,18 +312,23 @@ void move_bottom(Location *location) {
 }
 
 void clean(Location *location) {  
-    // move_left(location);
+    move_left(location);
     // move_bottom(location); 
     move_top(location); 
-    move_right(location);   
+    //move_right(location);   
     //TODO provide logic for cleaning rooms for AI-agent
 }
 
 void start(Location *location, clock_t end_time_millis) { 
+    srand(time(NULL));
     define_agent_coordinates(location);      
     do {         
         clean(location);
         display_matrix(location);
+        int rubbish_drop_probability = (int) rand() % (location->x_size * location->y_size);
+        if(rubbish_drop_probability > (location->x_size * location->y_size) / 2) {           
+            randomize_rubbish(location, 3, rand() % 10);
+        } 
         sleep(1);
     } while(clock() <= end_time_millis);
 }
--- location.h
+++ location.h
@@ -6,9 +6,19 @@
 
 static const char *CELL_VALUE[] = {" - ", "[X]", "{A}", "(R)"};
 
+typedef struct RubbishDefinition {   
+    int weight;
+} Rubbish;
+
+typedef struct AgentDefinition {   
+    int energy_count;
+} Agent;
+
 typedef struct CellDefinition {
     enum { ROOM, WALL, AGENT, RUBBISH } tag;
     const char *value;
+    Rubbish rubbish;
+    Agent agent;
 } Cell;
 
 typedef struct LocationDefinition {
@@ -24,16 +34,16 @@ void free_location(Location *location);
 
 void init_matrix(Location *location);
 
-void file_write_matrix(Location *location, char *file_path,
-    int walls_count, int rubbish_count, int players_count);
+void file_write_matrix(Location *location, char *file_path, int walls_count,
+    int rubbish_count, int players_count);
 
 void file_read_matrix(Location *location, char *file_path); 
 
 void randomize_matrix(Location *location, int walls_count);
 
-void randomize_rubbish(Location *location, int rubbish_count);
+void randomize_rubbish(Location *location, int rubbish_count, int weight);
 
-void init_agent(Location *location, int players_count);
+void init_agent(Location *location, int players_count, int energy_count);
 
 void display_matrix(Location *location);
 
